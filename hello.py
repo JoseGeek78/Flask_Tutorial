9f30ee2cf4231e37c686cbf5396dce502915f97b
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-23 13:28:04 +0100

    Updated

--- hello.py
+++ hello.py
@@ -3,10 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return '<h1>Página de inicio</h1>'
-
-@app.route('/hello/<name>/<int:age>')
 def hello(name, age):
     return f'<h1>Hola, {name}, tu edad es {age}!</h1>'
